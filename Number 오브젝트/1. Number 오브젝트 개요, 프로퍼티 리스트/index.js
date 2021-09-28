28eae4357f94c6d2c56b37139361bfd1c1d1dac5
IL KYO OH <81012135+qdaszx@users.noreply.github.com>
2021-09-29 04:05:01 +0900

    1. Number 오브젝트 개요, 프로퍼티 리스트

---
+++ Number 오브젝트/1. Number 오브젝트 개요, 프로퍼티 리스트/index.js
@@ -0,0 +1,26 @@
+/**
+ * Number 오브젝트
+ * - 숫자(123) 처리를 위한 오브젝트
+ * - 즉, Number 오브젝트에
+ *    - 숫자 처리를 위한
+ *    - 함수와 프로퍼티가 포함되어 있으며
+ *    - 함수를 호출하여 숫자 처리를 하게 됩니다.
+ */
+
+/**
+ * 프로퍼티 리스트
+ * 이름   / 개요
+ * new Number() / 인스턴스 생성
+ *
+ * Number 함수
+ * Number() / 숫자 값으로 변환하여 반환
+ *
+ * Number.prototype
+ * constructor  / 생성자
+ * toString() / 숫자 값을 문자열로 변환
+ * toLocaleString() / 숫자 값을 지역화 문자로 변환
+ * valueOf()  / 프리미티브 값 반환
+ * toExponential()  / 지수 표기로 변환
+ * toFixed()  / 고정 소숫점 표기로 변환
+ * toPrecision()  / 고정 소숫점 또는 지수 표기로 변환
+ */
