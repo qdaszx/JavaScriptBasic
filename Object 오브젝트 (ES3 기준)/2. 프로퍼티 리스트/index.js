0b4fecacd4b4df8f5f3424b0789f418e1d4b24a6
IL KYO OH <81012135+qdaszx@users.noreply.github.com>
2021-10-05 16:31:00 +0900

    2. 프로퍼티 리스트

---
+++ Object 오브젝트 (ES3 기준)/2. 프로퍼티 리스트/index.js
@@ -0,0 +1,15 @@
+/**
+ * 프로퍼티 리스트 (ES3)
+ * 이름 - 개요
+ * new Object() - 파라미터 데이터 타입의 인스턴스 생성
+ * Object() - Object 인스턴스 생성
+ *
+ * Obect.prototype
+ * constructor - 생성자
+ * valueOf() - 프리미티브 값 반환
+ * hasOwnPropert() - 프로퍼티 소유 여부 반환
+ * propertyIsEnumerable() - 프로퍼티 열거 여부 반환
+ * isPrototypeOf() - prototype의 존재 여부 반환
+ * toString() - 문자열로 변환
+ * toLocaleString() - 지역화 문자열로 변환
+ */
