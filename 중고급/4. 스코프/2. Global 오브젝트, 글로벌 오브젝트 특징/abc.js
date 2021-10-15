3b6010ccfe9d1541294ea35add92c15e152d518b
IL KYO OH <81012135+qdaszx@users.noreply.github.com>
2021-10-15 20:37:38 +0900

    2. Global 오브젝트, 글로벌 오브젝트 특징

---
+++ 중고급/4. 스코프/2. Global 오브젝트, 글로벌 오브젝트 특징/abc.js
@@ -0,0 +1,5 @@
+console.log("[코드2] <script src=\".\/abc.js\">");
+var value2 = 100;
+function book2() {
+  return value + 50;
+}
---
+++ 중고급/4. 스코프/2. Global 오브젝트, 글로벌 오브젝트 특징/def.js
@@ -0,0 +1,3 @@
+console.log("[코드3] <script src=\".\/def.js\">");
+console.log(value2);
+console.log(book2());
