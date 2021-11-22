6546c70365d9ac3c154f00658e9dde92b18e3133
IL KYO OH <81012135+qdaszx@users.noreply.github.com>
2021-11-22 13:08:22 +0900

    3. 비트 이동 연산자: 비트 NOT 연산자, << 연산자, >> 연산자, >>> 연산자, [정리 시간]

---
+++ ES6+ 심화/8. 비트(Bit) 연산자/3. 비트 이동 연산자: 비트 NOT 연산자, << 연산자, >> 연산자, >>> 연산자, [정리 시간]/index.js
@@ -0,0 +1,16 @@
+console.log(~5);
+console.log(~-5);
+
+const value = 3;
+console.log(value << 1);
+
+console.log(value << 2);
+
+const value2 = 10;
+console.log(value2 >> 1);
+console.log(value2 >> 2);
+
+var value3 = -10;
+
+console.log(value3 >> 1);
+console.log(value3 >> 2);
