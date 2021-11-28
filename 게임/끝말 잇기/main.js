e1684238d6790a67a901b5b3c21b00980b668dc8
IL KYO OH <81012135+qdaszx@users.noreply.github.com>
2021-11-28 13:16:14 +0900

    끝말 잇기 1차
    계속해서 개선해보기
    1. 한글
    2. 단어 유효성
    3. 테스트 코드
    4. 클린코드
    5. prototype 체인
    6. this

--- 게임/끝말 잇기/main.js
+++ 게임/끝말 잇기/main.js
@@ -1,7 +1,45 @@
 const number = Number(prompt("몇 명이 참가하나요?"));
+const total = document.querySelector("#total");
+const order = document.querySelector("#order");
+const input = document.querySelector("input")
+const button = document.querySelector("button")
+let word = document.querySelector("#word");
+let newWord;
 
-/**
- * 태그.addEventListener('이벤트 이름', 리스너 함수(콜백 함수));
- */
-document.querySelector("input").addEventListener("input", function () {});
-document.querySelector("button").addEventListener("click", function () {});
+!number ? location.reload() :
+
+  total.innerHTML = number;
+
+input.addEventListener("input", function (e) {
+  newWord = e.target.value;
+})
+
+const emptyInputValue = () => {
+  input.value = ""
+  input.focus();
+}
+
+const fail = () => {
+  alert(`${Number(order.innerHTML)}번째 참가자가 틀렸습니다.`);
+  emptyInputValue()
+}
+
+const nextStep = () => {
+  word.innerHTML = newWord;
+  emptyInputValue()
+
+  Number(order.innerHTML) + 1 > number ? order.innerHTML = 1 : order.innerHTML = Number(order.innerHTML) + 1
+}
+
+button.addEventListener("click", function () {
+  if (!input.value || !input.value.trim()) {
+    alert("제시어를 입력해주세요")
+    return input.focus();
+  }
+
+  if (!word.innerHTML) {
+    return nextStep()
+  }
+
+  input.value[0] === word.innerHTML[word.innerHTML.length - 1] ? nextStep() : fail()
+});
