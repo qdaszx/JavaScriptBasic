a6cb0e8e3318b058c1f1c2325418f7467b51da76
IL KYO OH <qdaszx@naver.com>
2022-01-28 00:13:58 +0900

    모듈화 - 이벤트 버블링
    삭제 버튼을 넣으면 불편하게 매번 랜더링마다 삭제에 관련한 이벤트를 붙여줘야 한다 그러한 불편함을 해결해줄 수 있는 것이 이벤트 버블링

--- 연습/웹컴포넌트/src/components/Items.js
+++ 연습/웹컴포넌트/src/components/Items.js
@@ -9,15 +9,29 @@ export default class Items extends Component {
     const { items } = this.$state
     return `
       <ul>
-        ${items.map(item => `<li>${item}</li>`).join('')}
+        ${items.map((item, key) => `
+          <li>
+            ${item}
+            <button class="deleteBtn" data-index="${key}">삭제</button>
+          </li>
+        `).join('')}
       </ul>
+      <button class="addBtn">추가</button>
     `
   }
 
   setEvent() {
-    this.$target.querySelector('button').addEventListener('click', () => {
-      const { items } = this.$state;
-      this.setState({ items: [...items, `item${items.length + 1}`] })
+    this.$target.addEventListener('click', ({ target }) => {
+      const items = [...this.$state.items]
+
+      if (target.classList.contains('addBtn')) {
+        this.setState({ items: [...items, `item${items.length + 1}`] })
+      }
+
+      if (target.classList.contains('deleteBtn')) {
+        items.splice(target.dataset.index, 1);
+        this.setState({ items });
+      }
     })
   }
 }
